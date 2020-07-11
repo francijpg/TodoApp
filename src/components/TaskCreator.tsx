9f631db9b0e3162fad9130cda9bc58a979631fcb
francijpg <hellofranciscopg@gmail.com>
2020-07-11 18:35:06 -0400

    first commit

---
+++ src/components/TaskCreator.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function TaskCreator() {
+  return (
+    <div>
+      <input type="text" autoFocus />
+      <button>Save</button>
+    </div>
+  );
+}
