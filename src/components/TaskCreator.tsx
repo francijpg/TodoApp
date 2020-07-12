8cc2924d32a79a32344afc46eff9f25c19b17d6a
francijpg <hellofranciscopg@gmail.com>
2020-07-12 00:07:00 -0400

    useState on input

--- src/components/TaskCreator.tsx
+++ src/components/TaskCreator.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TaskCreator() {
+  type FormElemEvent = React.FormEvent<HTMLFormElement>;
+
+  const [newTask, setNewTask] = useState<string>("");
+
+  const handlerSubmit = (e: FormElemEvent): void => {
+    e.preventDefault();
+    console.log(newTask);
+  };
+
   return (
     <div>
-      <input type="text" autoFocus />
-      <button>Save</button>
+      <form onSubmit={handlerSubmit}>
+        <input
+          type="text"
+          autoFocus
+          required
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button>Save</button>
+      </form>
     </div>
   );
 }
