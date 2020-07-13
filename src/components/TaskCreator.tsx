ae9ba0c7cd81524690235884106e0768b750fb61
francijpg <hellofranciscopg@gmail.com>
2020-07-13 01:08:17 -0400

    add todo list simple style

--- src/components/TaskCreator.tsx
+++ src/components/TaskCreator.tsx
@@ -29,29 +29,41 @@ const TaskCreator: React.FC = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handlerSubmit}>
-        <input
-          type="text"
-          autoFocus
-          required
-          value={newTask}
-          onChange={(e) => setNewTask(e.target.value)}
-        />
-        <button>Save</button>
-      </form>
-      {tasks.map((t: ITask, i: number) => (
-        <div key={i}>
-          <h1 style={{ textDecoration: t.done ? "line-through" : "" }}>
-            {t.name}
-          </h1>
-          <button onClick={() => toogleDoneTast(i)}>
-            {t.done ? "TODO" : "DONE"}
-          </button>
+    <div className="container p-4">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <div className="card">
+            <div className="card-header">
+              <h2 className="text-center">TODO LIST</h2>
+            </div>
+            <div className="card-body">
+              <form onSubmit={handlerSubmit}>
+                <input
+                  type="text"
+                  autoFocus
+                  required
+                  value={newTask}
+                  onChange={(e) => setNewTask(e.target.value)}
+                  className="form-control"
+                />
+                <button className="btn btn-success btn-block mt-2">Save</button>
+              </form>
+              {tasks.map((t: ITask, i: number) => (
+                <div key={i}>
+                  <h1 style={{ textDecoration: t.done ? "line-through" : "" }}>
+                    {t.name}
+                  </h1>
+                  <button onClick={() => toogleDoneTast(i)}>
+                    {t.done ? "TODO" : "DONE"}
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-      ))}
+      </div>
     </div>
   );
-}
+};
 
 export default TaskCreator;
