27dcc17153542e225c3c14467213965cd62737f3
francijpg <hellofranciscopg@gmail.com>
2020-07-17 16:08:56 -0400

    todos with table style

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -6,18 +6,20 @@ interface TodoListProps {
   removeTodo: ToggleDone;
 }
 
-export const TodoList: React.FC<TodoListProps> = ({ todos, toggleDone, removeTodo }) => {
-
+export const TodoList: React.FC<TodoListProps> = ({
+  todos,
+  toggleDone,
+  removeTodo,
+}) => {
   return (
     <div>
       {todos.map((t: Todo, i: number) => (
-        
         <div key={i}>
-          <h3 style={{ textDecoration: t.done ? "line-through" : "" }}>
+          {/* <h3 style={{ textDecoration: t.done ? "line-through" : "" }}>
             {t.name}
           </h3>
           <button
-            onClick={() => toggleDone(i) }
+            onClick={() => toggleDone(i)}
             className="btn btn-outline-success"
           >
             {t.done ? "TODO" : "DONE"}
@@ -27,7 +29,36 @@ export const TodoList: React.FC<TodoListProps> = ({ todos, toggleDone, removeTod
             className="btn btn-outline-danger"
           >
             Delete
-          </button>
+          </button> */}
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th>Task</th>
+                <th className="float-right">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="table-default">
+                <td style={{ textDecoration: t.done ? "line-through" : "" }}>
+                  <p className="text-break text-capitalize">{t.name}</p>
+                </td>
+                <td className="float-right">
+                  <button
+                    onClick={() => toggleDone(i)}
+                    className="btn btn-outline-success badge-pill"
+                  >
+                    {t.done ? "TODO" : "DONE"}
+                  </button>
+                  <button
+                    onClick={() => removeTodo(i)}
+                    className="btn btn-outline-danger badge-pill ml-1"
+                  >
+                    X
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       ))}
     </div>
