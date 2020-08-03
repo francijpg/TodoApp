cc290ca5c3e9f3a7a2a9e6a7e8dc95e3247b00b2
francijpg <hellofranciscopg@gmail.com>
2020-08-03 00:17:31 -0400

    addTodo test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { shallow, configure } from "enzyme";
+import App, { useTodos } from "./App";
+import { configure, shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 import TodoCreator from "./components/TodoCreator";
+
 configure({ adapter: new EnzymeAdapter() });
 
 describe("App", () => {
@@ -19,5 +21,21 @@ describe("App", () => {
       expect(addTodo.mock.calls).toEqual([[mockNewTodo]]);
       expect(prevent.mock.calls).toEqual([[]]);
     });
+  });
+
+  describe("custom hook: useTodos", () => {
+    it("addTodo", () => {
+      const Test = (props: any) => {
+        const hook = props.hook();
+        return <div {...hook} />;
+      };
+      const mockNewTodo = "new task";
+      const wrapper = shallow(<Test hook={useTodos} />);
+      let props: any = wrapper.find("div").props();
+      props.addTodo(mockNewTodo);
+      props = wrapper.find("div").props();
+      expect(props.todos[2].name).toEqual(mockNewTodo);
+    });
+
   });
 });
